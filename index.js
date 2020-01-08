585279ad73a33628adca972b2434e7bfc42a8afe
Gibriil <musicmandanceman@yahoo.com>
2020-01-08 14:48:33 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const port = 5000;
+
+const app = express();
+
+app.get('/', (req, res, next) => {
+    res.send(`
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>Simple Express Server</title>
+            </head>
+            <body>
+                <h1>Welcome to this Simple Express.js Node Server</h1>
+            </body>
+        </html>
+    `)
+})
+
+app.listen(port);
